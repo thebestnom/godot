26fd9e69d51d93afe2bb816815999c8d1d71d003
thebestnom <shoval.arad@gmail.com>
2020-03-03 23:28:24 +0200

    can debug!
    remove useles

--- platform/android/detect.py
+++ platform/android/detect.py
@@ -155,6 +155,8 @@ def configure(env):
         env.Append(CCFLAGS=['-O0', '-g', '-fno-limit-debug-info'])
         env.Append(CPPDEFINES=['_DEBUG', 'DEBUG_ENABLED', 'DEBUG_MEMORY_ENABLED'])
         env.Append(CPPFLAGS=['-UNDEBUG'])
+        env.Append(NDK_DEBUG=1)
+		
 
     # Compiler configuration
 
